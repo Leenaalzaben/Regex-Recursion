ab43b3c08dd922d979a02c98360b7429b69c8ff5
leenalzaben <leena.zaben61@gmail.com>
2023-03-14 18:52:44 +0000

    recursion-part

--- recursion.test.js
+++ recursion.test.js
@@ -2,7 +2,18 @@
 
 function division(number, dividedBy) {
     // Write you logic here.
-    return;
+    var result=0;
+    // base case return 0
+    while(number >= dividedBy){
+        number-=dividedBy;
+        result++;
+    }
+    return result;
+    // if (number < dividedBy) {
+    //     return 0;
+    // }else {
+    
+    // return 1 + division(number - dividedBy, dividedBy);
 }
 
 /* Write a function that implement Math.pow(x,n) but using recursion
@@ -13,7 +24,16 @@ pow(2,4) = 16
 
 function pow(x, n) {
     // Write you logic here.
-    return;
+    //X^0 =1
+    if(n==0){
+    return 1;
+    }
+    else if(n <0){
+        return 1 / pow(x, -n);
+    }else if(n%2 ==0){
+        return pow(x, n / 2) * pow(x, n / 2);
+    }
+    return  x * pow(x, n - 1);
 }
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
@@ -27,8 +47,16 @@ Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n) {
     // Write you logic here.
-    return;
-}
+    
+        if (n==1 || n==0){
+            return n
+        }else{
+            return  fibonacci(n-1) + fibonacci(n-2)
+        }
+    }
+    
+   
+
 
 /* Optional 
 
